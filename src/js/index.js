e5e847f1a97f112545898bef1536e4659cecc1ea
Alexsandro <alexsandrojunior53@gmail.com>
2023-01-02 17:17:12 -0300

    javascript finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,27 @@
 const input = document.querySelector('.input');
-const errorMessage = document.querySelector('.invalid-message');
+const errorNotice = document.querySelector('.invalid-message');
 const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-document.getElementById('btn').addEventListener('click',(event)=>{
+document.getElementById('form').addEventListener('submit',(event)=>{
     event.preventDefault();
-    if (input.value === '' || !input.value.match(emailFormat) ) {
-        errorMessage.style.display = 'block'
-        input.classList.add('input-invalid')
+    if (input.value === '') {
+        inputInvalid()
         input.placeholder = 'exemple@email/com'  
+        errorNotice.innerHTML = 'Whoops! It looks like you forgot to add your email'
+    } else if (!input.value.match(emailFormat)) {
+        inputInvalid()
+        errorNotice.innerHTML = 'Please provide a valid email address'
     } else {
-        errorMessage.style.display = 'none'
-        input.classList.remove('input-invalid')
+        inputValid()
     }
 });
+
+function inputInvalid(){
+    errorNotice.style.display = 'block'
+    input.classList.add('input-invalid')
+}
+
+function inputValid(){
+    errorNotice.style.display = 'none'
+    input.classList.remove('input-invalid')
+}
